f8500135eb8980e3d6d989c57c893756dad3a4ce
Gaurav Yadav <gauravyadav_2k19ec060@dtu.ac.in>
2022-01-02 22:35:11 +0530

    Update Count Digits in a number.java

--- DSA Lvl1/Basic Programming/Getting started/Count Digits in a number.java
+++ DSA Lvl1/Basic Programming/Getting started/Count Digits in a number.java
@@ -3,8 +3,8 @@ import java.util.*;
   public class Main{
   
   public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
-      int n = sc.nextInt();
+      Scanner scn = new Scanner(System.in);
+      int n = scn.nextInt();
 
       int count = 0;
       while(n != 0){
