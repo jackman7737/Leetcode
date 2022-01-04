9e913f273e7640810d24f08d7444bccb56aef893
Gaurav Yadav <gauravyadav_2k19ec060@dtu.ac.in>
2022-01-05 00:06:39 +0530

    Create Benjamin bulbs.java

---
+++ DSA Lvl1/Basic Programming/Getting started/Benjamin bulbs.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+  
+  public class benjaminbulbs{
+  
+  public static void main(String[] args) {
+    // write your code here  
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    for(int i = 1; i*i <= n; i++){
+        System.out.println(i*i);
+    }
+    sc.close();
+   }
+  }
