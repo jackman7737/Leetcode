ac332ab57bd74eee51921638be619d1a0b307660
Gaurav Yadav <gauravyadav_2k19ec060@dtu.ac.in>
2022-03-08 18:02:42 +0530

    Create subsequence.java

---
+++ DSA Lvl1/Recursion/onthewayup/subsequence.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        Scanner scan=new Scanner(System.in);
+        String ques= scan.nextLine();
+        printSS(ques,"");
+    }
+    public static void printSS(String str, String ans){
+        if(str.length()==0){
+            System.out.println(ans);
+            return;
+        }
+        char ch=str.charAt(0);
+        String req= str.substring(1);
+        printSS(req,ans+ch);
+        printSS(req,ans+"");
+    }
+
+}
